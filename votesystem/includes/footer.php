5aad447218c368fdd61211ee6fbe7250080630b0
Charitychacha <144662152+Charitychacha@users.noreply.github.com>
2023-12-12 21:01:30 +0200

    second commit
    voting system

---
+++ votesystem/includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2023 Charity Ngoma></strong>
+    </div>
+    <!-- /.container -->
+</footer>
